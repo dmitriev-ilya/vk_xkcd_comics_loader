5ccbe7f80592c2222d59ced6302eefb0d4fcbc97
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2022-12-18 12:27:26 +0500

    Проведена декомпозиция функции

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from xkdc_functions import get_xkcd_info, get_xkcd_image_name, download_xkdc_image
-from vk_wall_publisher_functions import publish_vk_wall_post
+from vk_wall_publisher_functions import (
+    get_upload_url,
+    upload_image_on_vk_server,
+    save_image_in_vk_group,
+    publish_vk_wall_post,
+)
 import random
 from environs import Env
 import os
@@ -8,19 +13,22 @@ import os
 if __name__ == "__main__":
     env = Env()
     env.read_env()
-
     group_id = env.int("VK_GROUP_ID")
     access_token = env.str("VK_APP_ACCESS_TOKEN")
+
     try:
         total_xkcd = get_xkcd_info()["num"]
         comics_number = random.randint(1, total_xkcd)
-        xkcd_response = get_xkcd_info(comics_number)
+        xkcd_info = get_xkcd_info(comics_number)
 
-        image_url = xkcd_response["img"]
+        image_url = xkcd_info["img"]
         image_name = get_xkcd_image_name(image_url)
-        post_text = xkcd_response["alt"]
+        post_text = xkcd_info["alt"]
         download_xkdc_image(image_url)
-    
-        publish_vk_wall_post(group_id, access_token, image_name, post_text)
+
+        upload_url = get_upload_url(group_id, access_token)
+        upload_info = upload_image_on_vk_server(upload_url, image_name)
+        save_method_info = save_image_in_vk_group(group_id, access_token, upload_info)
+        publish_vk_wall_post(group_id, access_token, save_method_info, post_text)
     finally:
         os.remove(image_name)
--- vk_wall_publisher_functions.py
+++ vk_wall_publisher_functions.py
@@ -1,9 +1,7 @@
 import requests
-from environs import Env
-import argparse
 
 
-def publish_vk_wall_post(group_id, access_token, image, text):
+def get_upload_url(group_id, access_token):
     upload_server_params = {
         "group_id": group_id,
         "access_token": access_token,
@@ -14,35 +12,45 @@ def publish_vk_wall_post(group_id, access_token, image, text):
         upload_server_url, params=upload_server_params
     )
     upload_server_response.raise_for_status()
+    upload_url = upload_server_response.json()["response"]["upload_url"]
+    return upload_url
+
+
+def upload_image_on_vk_server(upload_url, image):
     with open(image, "rb") as file:
-        upload_url = upload_server_response.json()["response"]["upload_url"]
         files = {
             "photo": file,
         }
         upload_response = requests.post(upload_url, files=files)
     upload_response.raise_for_status()
-    upload_response = upload_response.json()
+    upload_info = upload_response.json()
+    return upload_info
 
-    save_wall_method_data = {
+
+def save_image_in_vk_group(group_id, access_token, upload_info):
+    save_method_data = {
         "group_id": group_id,
-        "photo": upload_response["photo"],
-        "server": upload_response["server"],
-        "hash": upload_response["hash"],
+        "photo": upload_info["photo"],
+        "server": upload_info["server"],
+        "hash": upload_info["hash"],
         "access_token": access_token,
         "v": 5.131,
     }
     save_wall_method_url = "https://api.vk.com/method/photos.saveWallPhoto"
     save_wall_method_response = requests.post(
-        save_wall_method_url, data=save_wall_method_data
+        save_wall_method_url, data=save_method_data
     )
     save_wall_method_response.raise_for_status()
-    save_wall_method_response = save_wall_method_response.json()["response"][0]
+    save_method_info = save_wall_method_response.json()["response"][0]
+    return save_method_info
 
+
+def publish_vk_wall_post(group_id, access_token, save_method_info, text):
     post_method_url = "https://api.vk.com/method/wall.post"
     post_method_params = {
         "owner_id": -group_id,
         "from_group": 1,
-        "attachments": f"photo{save_wall_method_response['owner_id']}_{save_wall_method_response['id']}",
+        "attachments": f"photo{save_method_info['owner_id']}_{save_method_info['id']}",
         "message": text,
         "access_token": access_token,
         "v": 5.131,
