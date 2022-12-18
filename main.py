32a4293c3b34330cf82d87be8f5753a8eab943d8
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2022-12-18 11:39:19 +0500

    Добавлена очистка файлов в случае ошибки

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
 
     group_id = env.int("VK_GROUP_ID")
     access_token = env.str("VK_APP_ACCESS_TOKEN")
+    try:
+        total_xkcd = get_xkcd_info()["num"]
+        comics_number = random.randint(1, total_xkcd)
+        xkcd_response = get_xkcd_info(comics_number)
 
-    total_xkcd = get_xkcd_info()["num"]
-    comics_number = random.randint(1, total_xkcd)
-    xkcd_response = get_xkcd_info(comics_number)
-
-    image_url = xkcd_response["img"]
-    image_name = get_xkcd_image_name(image_url)
-    post_text = xkcd_response["alt"]
-    download_xkdc_image(image_url)
-
-    publish_vk_wall_post(group_id, access_token, image_name, post_text)
-
-    os.remove(image_name)
+        image_url = xkcd_response["img"]
+        image_name = get_xkcd_image_name(image_url)
+        post_text = xkcd_response["alt"]
+        download_xkdc_image(image_url)
+    
+        publish_vk_wall_post(group_id, access_token, image_name, post_text)
+    finally:
+        os.remove(image_name)
