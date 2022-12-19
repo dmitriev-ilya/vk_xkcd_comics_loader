750ce1b05076367ceeeaa463605b4bb48b05c217
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2022-12-19 21:32:53 +0500

    Добавлена обработка ошибок VK API

--- vk_wall_publisher_functions.py
+++ vk_wall_publisher_functions.py
@@ -1,6 +1,15 @@
 import requests
 
 
+def catch_vk_api_exception(response):
+    vk_api_error = response.json().get('error')
+    if vk_api_error:
+        error_code = vk_api_error['error_code']
+        error_msg = vk_api_error['error_msg']
+        error_description = f'Error {error_code}: {error_msg}'
+        raise requests.HTTPError(error_description)
+
+
 def get_upload_url(group_id, access_token):
     upload_server_params = {
         "group_id": group_id,
@@ -12,6 +21,8 @@ def get_upload_url(group_id, access_token):
         upload_server_url, params=upload_server_params
     )
     upload_server_response.raise_for_status()
+    catch_vk_api_exception(upload_server_response)
+
     upload_url = upload_server_response.json()["response"]["upload_url"]
     return upload_url
 
@@ -23,8 +34,9 @@ def upload_image_on_vk_server(upload_url, image):
         }
         upload_response = requests.post(upload_url, files=files)
     upload_response.raise_for_status()
-    upload_params = upload_response.json()
+    catch_vk_api_exception(upload_response)
 
+    upload_params = upload_response.json()
     uploaded_photo_url = upload_params["photo"]
     upload_server_url = upload_params["server"]
     upload_hash = upload_params["hash"]
@@ -48,6 +60,8 @@ def save_image_in_vk_group(
         save_wall_method_url, data=save_method_sending
     )
     save_wall_method_response.raise_for_status()
+    catch_vk_api_exception(save_wall_method_response)
+
     save_method_params = save_wall_method_response.json()["response"][0]
     photo_owner_id = save_method_params['owner_id']
     photo_id = save_method_params['id']
@@ -72,3 +86,4 @@ def publish_vk_wall_post(
         params=post_method_params
     )
     post_method_response.raise_for_status()
+    catch_vk_api_exception(post_method_response)
